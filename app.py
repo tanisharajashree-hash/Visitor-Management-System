7167cc34e25a3390540123344bf03fa8c9728326
Tanisha P <tanisharajashree@gmail.com>
2026-09-05 19:01:29 +0530

    Updated Login and Dashboard

--- app.py
+++ app.py
@@ -45,6 +45,116 @@ def login():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route("/visitors")
+def visitors():
+
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM visitor")
+
+    visitors = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template("visitors.html", visitors=visitors)
+
+@app.route("/view-visitor/<int:visitor_id>")
+def view_visitor(visitor_id):
+
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute(
+        "SELECT * FROM visitor WHERE visitor_id = %s",
+        (visitor_id,)
+    )
+
+    visitor = cursor.fetchone()
+
+    cursor.close()
+
+    return render_template("view_visitor.html", visitor=visitor)
+
+@app.route("/edit-visitor/<int:visitor_id>", methods=["GET", "POST"])
+def edit_visitor(visitor_id):
+
+    cursor = db.cursor(dictionary=True)
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        phone = request.form["phone"]
+        email = request.form["email"]
+        purpose = request.form["purpose"]
+
+        cursor.execute(
+            """
+            UPDATE visitor
+            SET name = %s,
+                phone = %s,
+                email = %s,
+                purpose = %s
+            WHERE visitor_id = %s
+            """,
+            (name, phone, email, purpose, visitor_id)
+        )
+
+        db.commit()
+        cursor.close()
+
+        return redirect(url_for("visitors"))
+
+    cursor.execute(
+        "SELECT * FROM visitor WHERE visitor_id = %s",
+        (visitor_id,)
+    )
+
+    visitor = cursor.fetchone()
+
+    cursor.close()
+
+    return render_template("edit_visitor.html", visitor=visitor)
+
+
+@app.route("/delete-visitor/<int:visitor_id>")
+def delete_visitor(visitor_id):
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "DELETE FROM visitor WHERE visitor_id = %s",
+        (visitor_id,)
+    )
+
+    db.commit()
+    cursor.close()
+
+    return redirect(url_for("visitors"))
+
+@app.route("/add-visitor", methods=["GET", "POST"])
+def add_visitor():
+
+    if request.method == "POST":
+        name = request.form["name"]
+        phone = request.form["phone"]
+        email = request.form["email"]
+        purpose = request.form["purpose"]
+
+        cursor = db.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO visitor (name, phone, email, purpose, admin_id)
+            VALUES (%s, %s, %s, %s, %s)
+            """,
+            (name, phone, email, purpose, 1)
+        )
+
+        db.commit()
+        cursor.close()
+
+        return redirect(url_for("visitors"))
+
+    return render_template("add_visitor.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
